81392741286522ba2af0369582c732ad454ab806
plucury <plucury@gmail.com>
2018-04-18 18:30:32 +0800

    fixup

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools import setup, find_packages
 setup(name="django-smart-selects",
       version="1.0.4",
       description="Django application to handle chained model fields.",
-      long_description=long_desc,
       author="Patrick Lauber",
       author_email="digi@treepy.com",
       url="https://github.com/digi604/django-smart-selects",
